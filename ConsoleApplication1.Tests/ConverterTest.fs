e0fa3de9e135577aace89a960731a44e68578e94
Victor Bartel <victor.bartel@dgtresor.gouv.fr>
2016-03-25 14:26:55 +0100

    Add first tests

---
+++ ConsoleApplication1.Tests/ConverterTest.fs
@@ -0,0 +1,21 @@
+﻿
+namespace Tests
+
+open Xunit
+open Swensen.Unquote
+
+type ConverterTest1() =
+    
+    [<Fact>]
+    member me.``It should convert meters to centimeters as expected``() =
+        
+        let actual = 1100.0<cm> |> centimetersToMeters
+
+        test <@ actual = 11.0<m> @>
+
+module ConverterTest2 =
+    [<Fact>]
+    let ``It should convert meters to feets as expected`` () =
+        let actual =  32.0<m> |> metersToFeets
+
+        test <@ actual = 104.9868766<ft> @>
